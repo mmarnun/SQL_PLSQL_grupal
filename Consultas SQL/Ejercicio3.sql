ce25590dd937aacfbfa813438cc3deb32adb933d
mmarnun <123729868+mmarnun@users.noreply.github.com>
2023-11-18 16:42:44 +0100

    Update Ejercicio3.sql

---
+++ Consultas SQL/Ejercicio3.sql
@@ -0,0 +1,10 @@
+-- Muestra para cada propietario de locales comerciales el importe total que adeuda a su
+-- comunidad, incluyendo a aquellos que están al corriente de pago actualmente.
+
+
+select p.dni, p.nombre, p.apellidos, l.codcomunidad, SUM(rc.importe) AS Importe_Total_Adeudado
+from propietarios p
+join propiedades pr on p.dni = pr.dni_propietario
+join locales l on pr.codcomunidad = l.codcomunidad and pr.codpropiedad = l.codpropiedad
+left join recibos_cuotas rc on l.codcomunidad = rc.codcomunidad and p.dni = rc.dni
+group by p.dni, p.nombre, p.apellidos, l.codcomunidad;
