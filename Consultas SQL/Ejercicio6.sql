937025d51a418235ca6affdb4e6a1302bb66a8bb
FMP24 <126695764+FMP24@users.noreply.github.com>
2023-11-18 18:23:44 +0100

    Update Ejercicio6.sql

---
+++ Consultas SQL/Ejercicio6.sql
@@ -0,0 +1,9 @@
+-- Muestra el porcentaje de inquilinos sobre el número total de propiedades que tiene cada comunidad, incluyendo a las que no tengan ninguno.
+
+SELECT c.codcomunidad, ROUND(NVL(COUNT(i.dni) * 100.0 / NULLIF(COUNT(p.codpropiedad), 0), 0), 2) || '%' AS porcentaje_inquilinos
+FROM comunidades c, propiedades p, inquilinos i 
+WHERE c.codcomunidad = p.codcomunidad (+)
+AND p.codpropiedad = i.codpropiedad (+)
+AND p.codcomunidad = i.codcomunidad (+)
+GROUP BY c.codcomunidad
+ORDER BY c.codcomunidad;
