40ecc17d8c2acf44054c2deb77383b2cd97d10fd
Juan Manuel Duran Piñero <98739764+Juanmanueldupi@users.noreply.github.com>
2023-11-21 10:19:21 +0100

    Update Ejercicio10.sql

--- Consultas SQL/Ejercicio10.sql
+++ Consultas SQL/Ejercicio10.sql
@@ -1,22 +1,28 @@
-CREATE VIEW vista_administradores AS
-SELECT A.nombre, A.apellidos, C.codcomunidad, COUNT(*) AS recibos_impagados
+CREATE OR REPLACE VIEW vista_administradores AS
+SELECT
+    A.nombre,
+    A.apellidos,
+    C.codcomunidad,
+    NVL(COUNT(RC.codcomunidad), 0) AS recibos_impagados
 FROM administradores A
 JOIN contratos_de_mandato CM ON A.numcolegiado = CM.numcolegiado
 JOIN comunidades C ON CM.codcomunidad = C.codcomunidad
-JOIN recibos_cuotas RC ON C.codcomunidad = RC.codcomunidad
-WHERE RC.pagado = 'No'
-GROUP BY A.nombre, A.apellidos, C.codcomunidad
-HAVING COUNT(*) = (
-    SELECT MAX(cnt)
-    FROM (
-        SELECT C.codcomunidad, COUNT(*) AS cnt
-        FROM administradores A
-        JOIN contratos_de_mandato CM ON A.numcolegiado = CM.numcolegiado
-        JOIN comunidades C ON CM.codcomunidad = C.codcomunidad
-        JOIN recibos_cuotas RC ON C.codcomunidad = RC.codcomunidad
-        WHERE RC.pagado = 'No'
-        GROUP BY C.codcomunidad
+LEFT JOIN recibos_cuotas RC ON C.codcomunidad = RC.codcomunidad AND RC.pagado = 'No'
+WHERE (C.codcomunidad, A.numcolegiado) IN (
+        SELECT codcomunidad, numcolegiado
+        FROM (
+            SELECT
+                C.codcomunidad,
+                A.numcolegiado,
+                RANK() OVER (PARTITION BY A.numcolegiado ORDER BY COUNT(RC.codcomunidad) DESC) AS rnk
+            FROM administradores A
+            JOIN contratos_de_mandato CM ON A.numcolegiado = CM.numcolegiado
+            JOIN comunidades C ON CM.codcomunidad = C.codcomunidad
+            LEFT JOIN recibos_cuotas RC ON C.codcomunidad = RC.codcomunidad AND RC.pagado = 'No'
+            GROUP BY C.codcomunidad, A.numcolegiado
+        )
+        WHERE rnk = 1
     )
-);
+GROUP BY A.nombre, A.apellidos, C.codcomunidad;
 
 --Juan Manuel
